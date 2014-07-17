dec20e2b9d4eb54365a19bd014b16cd3b67e2b60
Coolway99 <xxcoolwayxx@gmail.com>
2014-07-17 13:09:37 -0400

    Changed some things, added contex menu

--- main/ToS_Fourm_Mafia_Note_Taker/src/main/Main.java
+++ main/ToS_Fourm_Mafia_Note_Taker/src/main/Main.java
@@ -11,21 +11,20 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import assets.ClassOfButtons;
-import assets.actionListeners.ButtonMouseListener;
+import assets.MainRightClickMenu;
 
 public class Main {
 	private static boolean[] keys = new boolean[65585];
 	private static JPanel mainPanel = new MainPanel();
 	private static JFrame frame;
-	public static JTextField textField = new JTextField();
+	public static JTextField textField1 = new JTextField();
+	public static JTextField textField2 = new JTextField();
 	public static Mouse mouse = new Mouse();
-	public static ButtonMouseListener clickyButton = new ButtonMouseListener();
 	private static GroupLayout layout = new GroupLayout(mainPanel);
 	
 	public static void main(String[] Args) throws InterruptedException{
+		MainRightClickMenu.initPopup();
 		ClassOfButtons.init();
-		ClassOfButtons.testbutton1.addMouseListener(clickyButton);
-		ClassOfButtons.testbutton2.addMouseListener(clickyButton);
 		frame = new JFrame("Test Window");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.addMouseListener(mouse);
@@ -36,19 +35,20 @@ public class Main {
 		GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
 		GroupLayout.SequentialGroup vGroup = layout.createSequentialGroup();
 		vGroup.addGap(200).addGroup(layout.createParallelGroup().addComponent(ClassOfButtons.
-				testbutton1, 25, 25, 25).addComponent(textField, 25, 25, 25).addComponent(ClassOfButtons.
-						testbutton2, 25, 25, 25)).addGap(375);
+				testbutton1, 25, 25, 25).addComponent(textField1, 25, 25, 25).addComponent(ClassOfButtons.
+						testbutton2, 25, 25, 25)).addComponent(textField2, 25, 25, 25).addGap(350);
 		hGroup.addGap(200).addComponent(ClassOfButtons.testbutton1, 25, 25, 25).addGap(75)
-		.addComponent(textField, 200, 200, 200).addGap(75).addComponent(ClassOfButtons.testbutton2, 25, 25,
-				25).addGap(200);
+		.addGroup(layout.createParallelGroup().addComponent(textField1, 200, 200, 200).
+				addComponent(textField2, 200, 200, 200)).addGap(75).addComponent(ClassOfButtons.
+						testbutton2, 25, 25, 25).addGap(200);
 		layout.setVerticalGroup(vGroup);
 		layout.setHorizontalGroup(hGroup);
 		frame.add(mainPanel);
 		frame.pack();
 		frame.setResizable(false);
 		frame.setVisible(true);
-		textField.setText("I am a simple uneditable testbox");
-		textField.setEditable(true);
+		textField1.setText("I am a simple uneditable testbox");
+		textField1.setColumns(20);
 		KeyEventDispatcher keyEventA = new KeyEventDispatcher(){
 			@Override
 			public boolean dispatchKeyEvent(KeyEvent e){
