21d738681f957b545f7aac263d347c104d8f3fb5
Coolway99 <xxcoolwayxx@gmail.com>
2014-07-17 13:09:00 -0400

    Added Buttony stuff

--- main/ToS_Fourm_Mafia_Note_Taker/src/assets/ClassOfButtons.java
+++ main/ToS_Fourm_Mafia_Note_Taker/src/assets/ClassOfButtons.java
@@ -1,22 +1,27 @@
 package assets;
 
 import main.Main;
+import assets.actionListeners.ButtonMouseListener;
+
 
 public class ClassOfButtons {
+	public static ButtonMouseListener clickyButton = new ButtonMouseListener();
 	public static Buttons1 testbutton1, testbutton2;
 	@SuppressWarnings("serial")
 	public static void init(){
 		testbutton1 = new Buttons1(){
 			@Override 
 			public void pressed(int button){
-				Main.textField.setText("("+Integer.toString(button)+")Button1 Has been pressed");
+				Main.textField1.setText("("+Integer.toString(button)+")Button1 Has been pressed");
 			}
 		};
 		testbutton2 = new Buttons1(){
 			@Override 
 			public void pressed(int button){
-				Main.textField.setText("("+Integer.toString(button)+")Button2 Has been pressed");
+				Main.textField1.setText("("+Integer.toString(button)+")Button2 Has been pressed");
 			}
 		};
+		testbutton1.addMouseListener(clickyButton);
+		testbutton2.addMouseListener(clickyButton);
 	}
 }
