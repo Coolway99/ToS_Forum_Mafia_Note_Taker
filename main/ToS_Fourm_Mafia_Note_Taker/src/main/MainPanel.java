f61bb6d2757abc061cf82d2526245cbf069b3136
Coolway99 <xxcoolwayxx@gmail.com>
2014-07-16 22:20:33 -0400

    Renamed Panel to MainPanel for the sake of having more panels

--- main/ToS_Fourm_Mafia_Note_Taker/src/main/MainPanel.java
+++ main/ToS_Fourm_Mafia_Note_Taker/src/main/MainPanel.java
@@ -1,28 +1,18 @@
 package main;
 
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.util.HashMap;
 
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
-public class Panel extends JPanel{
-	private HashMap<Integer, Component> componentArray = new HashMap<Integer, Component>();
-	public Panel(){
+public class MainPanel extends JPanel{
+	public MainPanel(){
 		super();
 	}
 	public void update(){
 		
-	}
-	public void input(Component c){
-		if(componentArray.isEmpty()){
-			componentArray.put(0, c);
-		} else {
-			componentArray.put((componentArray.values().toArray(new Component[0]).length), c);
-		}
 	}
 	@Override
 	public void paintComponent(Graphics g){
@@ -36,8 +26,5 @@ public class Panel extends JPanel{
 	@Override
 	public Dimension getMaximumSize(){
 		return new Dimension(800, 600);
-	}
-	public void pressed(){
-		System.out.println("yay");
 	}
 }
