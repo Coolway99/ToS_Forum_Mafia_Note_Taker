31b35736ea23d93149343273bbaa2ca93a1f4724
Coolway99 <xxcoolwayxx@gmail.com>
2014-07-16 22:38:10 -0400

    Button booleans were unneeded

--- main/ToS_Fourm_Mafia_Note_Taker/src/assets/actionListeners/ButtonMouseListener.java
+++ main/ToS_Fourm_Mafia_Note_Taker/src/assets/actionListeners/ButtonMouseListener.java
@@ -7,16 +7,13 @@ import javax.swing.event.MouseInputAdapter;
 import assets.Buttons1;
 
 public class ButtonMouseListener extends MouseInputAdapter{
-	private boolean button1, button2;
 	@SuppressWarnings("static-access")
 	@Override
 	public void mousePressed(MouseEvent e) {
 		if(e.getButton() == e.BUTTON1){
-			button1 = true;
 			Buttons1 button = (Buttons1)e.getSource();
 			button.pressed(1);
 		} else if (e.getButton() == e.BUTTON2){
-			button2 = true;
 			Buttons1 button = (Buttons1)e.getSource();
 			button.pressed(2);
 		}
@@ -26,11 +23,9 @@ public class ButtonMouseListener extends MouseInputAdapter{
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		if(e.getButton() == e.BUTTON1){
-			button1 = false;
 			Buttons1 button = (Buttons1)e.getSource();
 			button.released(1);
 		} else if (e.getButton() == e.BUTTON2){
-			button2 = false;
 			Buttons1 button = (Buttons1)e.getSource();
 			button.released(2);
 		}
