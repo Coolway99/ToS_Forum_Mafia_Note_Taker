da52ed64f27a37635908ad61af9eb03908ade892
Coolway99 <xxcoolwayxx@gmail.com>
2014-07-16 22:21:33 -0400

    Changed due to other changes

--- main/ToS_Fourm_Mafia_Note_Taker/src/main/Main.java
+++ main/ToS_Fourm_Mafia_Note_Taker/src/main/Main.java
@@ -10,35 +10,34 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-import assets.actionListeners.Listener;
-
 public class Main {
 	private static boolean[] keys = new boolean[65585];
-	private static JPanel panel = new Panel();
+	private static JPanel mainPanel = new MainPanel();
 	private static JFrame frame;
 	private static JTextField textField = new JTextField();
 	public static Mouse mouse = new Mouse();
-	public static Listener listener = new Listener();
-	private static GroupLayout layout = new GroupLayout(panel);
+	private static GroupLayout layout = new GroupLayout(mainPanel);
 	
 	public static void main(String[] Args) throws InterruptedException{
 		frame = new JFrame("Test Window");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.addMouseListener(mouse);
 		frame.addMouseMotionListener(mouse);
-		panel.setLayout(layout);
+		mainPanel.setLayout(layout);
 		layout.setAutoCreateContainerGaps(false);
 		layout.setAutoCreateGaps(false);
 		GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
 		GroupLayout.SequentialGroup vGroup = layout.createSequentialGroup();
 		vGroup.addGap(200).addComponent(textField, 25, 25, 25).addGap(375);
-		hGroup.addGap(325).addComponent(textField, 150, 150, 150).addGap(325);
+		hGroup.addGap(300).addComponent(textField, 200, 200, 200).addGap(300);
 		layout.setVerticalGroup(vGroup);
 		layout.setHorizontalGroup(hGroup);
-		frame.add(panel);
+		frame.add(mainPanel);
 		frame.pack();
 		frame.setResizable(false);
 		frame.setVisible(true);
+		textField.setText("I am a simple uneditable testbox");
+		textField.setEditable(true);
 		KeyEventDispatcher keyEventA = new KeyEventDispatcher(){
 			@Override
 			public boolean dispatchKeyEvent(KeyEvent e){
@@ -54,8 +53,6 @@ public class Main {
 		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(keyEventA);
 		while(true){
 			frame.repaint();
-			textField.setText(null);
-			textField.setText("x = " + Integer.toString(mouse.getPoint().x) + ", y = " + Integer.toString(mouse.getPoint().y));
 			Thread.sleep(10);
 		}
 	}
