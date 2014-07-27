84335e055d19fa53d834c37e52baa3ffb4c33496
Coolway99 <xxcoolwayxx@gmail.com>
2014-07-27 10:50:49 -0400

    Moved Previous class to subclass, reworked everything SLIGHT BUG WITH REMOVE DAY

--- main/ToS_Fourm_Mafia_Note_Taker/src/assets/DayButtons.java
+++ main/ToS_Fourm_Mafia_Note_Taker/src/assets/DayButtons.java
@@ -1,23 +1,87 @@
 package assets;
 
 import java.awt.Component;
-import java.awt.Graphics;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusListener;
+import java.util.HashMap;
 
+import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.ParallelGroup;
 import javax.swing.GroupLayout.SequentialGroup;
 import javax.swing.JButton;
+import javax.swing.Timer;
+
+import main.Main;
 
 @SuppressWarnings("serial")
 public class DayButtons extends Component{
+	private HashMap<Integer, Buttons> buttonArray = new HashMap<Integer, Buttons>();
+	private Timer timer = new Timer(100, null);
+	public void init(){
+		buttonArray.put(0, new Buttons());
+		buttonArray.put(1, new Buttons(1));
+		this.setDayActionListener(new PrivateAddDayActionListener(), 0);
+		this.setNightActionListener(new PrivateRemoveDayActionListener(), 0);
+		timer.setRepeats(false);
+		timer.start();
+	}
+	public void setDayActionListener(ActionListener L, int day){
+		buttonArray.get(day).setDayActionListener(L);
+	}
+	public void setNightActionListener(ActionListener L, int day){
+		buttonArray.get(day).setNightActionListener(L);
+	}
+	public void createNewDay(){
+		if(!timer.isRunning()){
+			buttonArray.put(buttonArray.size(), new Buttons(buttonArray.size()));
+			Main.initLayout();
+			Main.frame.repaint();
+			timer.restart();
+		}
+	}
+	public void removeADay(){
+		if(!timer.isRunning()){
+			if(buttonArray.size() > 2){
+				buttonArray.remove(buttonArray.size()-1);
+				Main.initLayout();
+				Main.frame.repaint();
+				timer.restart();
+			}
+		}
+	}
+	public SequentialGroup setVerticalLocation(GroupLayout layout, int size){
+		SequentialGroup group = layout.createSequentialGroup();
+		ParallelGroup pGroup = layout.createParallelGroup();
+		for(int x = 1; x < buttonArray.size(); x++){
+			pGroup.addGroup(buttonArray.get(x).setVerticalLocation(layout.createSequentialGroup(), size, buttonArray.size()-1));
+		}
+		pGroup.addGroup(buttonArray.get(0).setVerticalLocation(layout.createSequentialGroup(), size));
+		return group.addGroup(pGroup);
+	}
+	public SequentialGroup setHorizontalLocation(GroupLayout layout, int sizeA, int sizeB){
+		SequentialGroup group = layout.createSequentialGroup();
+		for(int x = 1; x < buttonArray.size(); x++){
+			group.addGroup(layout.createParallelGroup()
+					.addGroup(buttonArray.get(x).setHorizontalLocation(layout.createSequentialGroup(), layout.createParallelGroup(), (sizeA/(buttonArray.size()-1)))));
+		}
+		group.addGroup(buttonArray.get(0).setHorizontalLocation(layout.createSequentialGroup(), layout.createParallelGroup(), sizeB));
+		return group;
+	}
+}
+
+class Buttons {
 	private JButton dayButton;
 	private JButton nightButton;
 	private int dayNumber;
-	public DayButtons(int DayNumber){
+	public Buttons(int DayNumber){
 		dayNumber = DayNumber;
 		dayButton = new JButton("Day "+Integer.toString(dayNumber));
 		nightButton = new JButton("Night "+Integer.toString(dayNumber));
+	}
+	public Buttons(){
+		dayNumber = 0;
+		dayButton = new JButton("Add Day");
+		nightButton = new JButton("Remove Day");
 	}
 	public int getDayNumber(){
 		return dayNumber;
@@ -27,12 +91,6 @@ public class DayButtons extends Component{
 	}
 	public void setNightActionListener(ActionListener L){
 		nightButton.addActionListener(L);
-	}
-	public void setDayFocusListener(FocusListener L){
-		dayButton.addFocusListener(L);
-	}
-	public void setNightFocusListener(FocusListener L){
-		nightButton.addFocusListener(L);
 	}
 	public SequentialGroup setVerticalLocation(SequentialGroup group, int size){
 		group.addComponent(dayButton, size/2, size/2, size/2)
@@ -45,9 +103,39 @@ public class DayButtons extends Component{
 				.addComponent(nightButton, size, size, size));
 		return group;
 	}
+	public SequentialGroup setVerticalLocation(SequentialGroup group, int size, int Amount){
+		if(Amount > 4){
+			if(Amount > 6){
+				dayButton.setText(Integer.toString(this.getDayNumber()));
+				dayButton.setFont(dayButton.getFont().deriveFont(8F));
+				nightButton.setText(Integer.toString(this.getDayNumber()));
+				nightButton.setFont(nightButton.getFont().deriveFont(8F));
+			} else {
+				dayButton.setText("D-"+this.getDayNumber());
+				nightButton.setText("N-"+this.getDayNumber());
+				dayButton.setFont(dayButton.getFont().deriveFont(12F));
+				nightButton.setFont(nightButton.getFont().deriveFont(12F));
+			}
+		} else {
+			dayButton.setText("Day "+this.getDayNumber());
+			nightButton.setText("Night "+this.getDayNumber());
+		}
+		return this.setVerticalLocation(group, size);
+	}
+}
+class PrivateTimerListener implements ActionListener{
 	@Override
-	public void paint(Graphics g){
-		dayButton.paint(g);
-		nightButton.paint(g);
+	public void actionPerformed(ActionEvent e){}
+}
+class PrivateAddDayActionListener implements ActionListener{
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Main.dayButtons.createNewDay();
+	}
+}
+class PrivateRemoveDayActionListener implements ActionListener{
+	@Override
+	public void actionPerformed(ActionEvent e){
+		Main.dayButtons.removeADay();
 	}
 }
