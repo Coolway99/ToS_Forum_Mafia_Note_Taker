815effdfc6a78eed172bdd18d44aa46b06d46f34
Coolway99 <xxcoolwayxx@gmail.com>
2014-07-27 14:56:41 -0400

    Created a base for the Save/Load feature

---
+++ main/ToS_Fourm_Mafia_Note_Taker/src/interfaces/SaveLoadListener.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+/**
+ * @param <V> The Value, commonly meant to be a List<V>
+ * @see List<V>
+ */
+public interface SaveLoadListener<V> {
+	public V gatherSaveData();
+	public void inputLoadData( V input);
+}
