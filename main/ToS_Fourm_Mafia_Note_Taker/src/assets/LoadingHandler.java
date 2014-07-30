d5688f4e8255485e77d89d76ad0cdca2f26fbdfd
Coolway99 <xxcoolwayxx@gmail.com>
2014-07-30 19:42:53 -0400

    Completly rewrote version comparer for less/greater O.o

--- main/ToS_Fourm_Mafia_Note_Taker/src/assets/LoadingHandler.java
+++ main/ToS_Fourm_Mafia_Note_Taker/src/assets/LoadingHandler.java
@@ -2,6 +2,8 @@ package assets;
 
 import java.util.HashMap;
 
+import javax.swing.JOptionPane;
+
 import main.Main;
 
 import org.xml.sax.Attributes;
@@ -33,7 +35,14 @@ public class LoadingHandler implements ContentHandler{
 				patch = atts.getValue("patch");
 				Version = saveV+"."+patch;
 				if(VersionParsingHandler.isVersionLessThan(Version, "0.0.1.0")){
-					
+					JOptionPane.showConfirmDialog(Main.frame, "WARNING! The save you are"
+							+ "loading is from a previous version, between then and now the"
+							+ "playerlist and graveyard have merged.\n\n"
+							+ "If you want to keep the graveyard and scrap the playerlist, push yes.\n"
+							+ "If you want to keep the playerlist and scrap the graveyard, push no\n"
+							+ "If you want to cancel the operation, please press \"Cancel\"","WARNING!", 
+							JOptionPane.YES_NO_CANCEL_OPTION,
+							JOptionPane.WARNING_MESSAGE);
 				}
 			} else if(qName.equals("number")){
 				day = Integer.parseInt(atts.getValue("day"));
@@ -183,22 +192,34 @@ class VersionParsingHandler{
 	public static boolean isVersionLessThan(String isThis, String lessThanThis){
 		int A[] = parseVersion(isThis);
 		int B[] = parseVersion(lessThanThis);
-		if(A[0] <= B[0]){
-			if(A[1] <= B[1]){
-				if(A[2] <= B[2]){
-					if(A[3] < B[3]){
-						return true;
+		if(A[0] < B[0]){
+			return true;
+		} else {
+			if(A[0] == B[0]){
+				if(A[1] < B[1]){
+					return true;
+				} else {
+					if(A[1] == B[1]){
+						if(A[2] < B[2]){
+							return true;
+						} else {
+							if(A[2] == B[2]){
+								if(A[3] < B[3]){
+									return true;
+								} else {
+									return false;
+								}
+							} else {
+								return false;
+							}
+						}
 					} else {
 						return false;
 					}
-				} else {
-					return false;
 				}
 			} else {
 				return false;
 			}
-		} else {
-			return false;
 		}
 	}
 	public static boolean isVersionEqualToo(String isThis, String equalToThis){
@@ -225,26 +246,38 @@ class VersionParsingHandler{
 	public static boolean isVersionGreaterThan(String isThis, String greaterThanThis){
 		int A[] = parseVersion(isThis);
 		int B[] = parseVersion(greaterThanThis);
-		if(A[0] >= B[0]){
-			if(A[1] >= B[1]){
-				if(A[2] >= B[2]){
-					if(A[3] > B[3]){
-						return true;
+		if(A[0] > B[0]){
+			return true;
+		} else {
+			if(A[0] == B[0]){
+				if(A[1] > B[1]){
+					return true;
+				} else {
+					if(A[1] == B[1]){
+						if(A[2] > B[2]){
+							return true;
+						} else {
+							if(A[2] == B[2]){
+								if(A[3] > B[3]){
+									return true;
+								} else {
+									return false;
+								}
+							} else {
+								return false;
+							}
+						}
 					} else {
 						return false;
 					}
-				} else {
-					return false;
 				}
 			} else {
 				return false;
 			}
-		} else {
-			return false;
 		}
 	}
 	private static int[] parseVersion(String Version){
-		String unParsed[] = Version.split(".");
+		String unParsed[] = Version.split("\\.");
 		int version[] = new int[unParsed.length];
 		for(int x = 0; x < version.length; x++){
 			version[x] = Integer.parseInt(unParsed[x]);
