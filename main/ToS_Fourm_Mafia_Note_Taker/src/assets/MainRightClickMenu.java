21a0cb21413416938eb94e21087921d67bfbefbf
Coolway99 <xxcoolwayxx@gmail.com>
2014-08-05 12:24:52 -0400

    Added BBCode->HTML, renamed global e to eBox

--- main/ToS_Fourm_Mafia_Note_Taker/src/assets/MainRightClickMenu.java
+++ main/ToS_Fourm_Mafia_Note_Taker/src/assets/MainRightClickMenu.java
@@ -2,13 +2,15 @@ package assets;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
-import javax.swing.JTextArea;
-import javax.swing.JTextPane;
+
+import main.Main;
 
 
 @SuppressWarnings("serial")
@@ -16,12 +18,16 @@ public class MainRightClickMenu extends JPopupMenu{
 	protected static JPopupMenu pop;
 	private static PersonalActionListener listener = new PersonalActionListener();
 	public static PersonalMouseListener mouse = new PersonalMouseListener();
-	protected static MouseEvent e;
+	protected static MouseEvent eBox;
+	public static FocusListener1 focus = new FocusListener1();
 	public static void initPopup(){
 		pop = new JPopupMenu();
 		MenuInterface menuItem = new MenuInterface("Edit");
 	    menuItem.addActionListener(listener);
 	    pop.add(menuItem);
+	    {
+
+	    }
 	}
 }
 class PersonalMouseListener extends MouseAdapter{
@@ -35,7 +41,7 @@ class PersonalMouseListener extends MouseAdapter{
 		if(e.getButton() == e.BUTTON3){
 			if(e.isPopupTrigger()){
 				MainRightClickMenu.pop.show(e.getComponent(), e.getX(), e.getY());
-				MainRightClickMenu.e = e;
+				MainRightClickMenu.eBox = e;
 			}
 		}
 	}
@@ -54,7 +60,39 @@ class MenuInterface extends JMenuItem{
 	public MenuInterface(String string) {super(string);}
 
 	public void doAction(ActionEvent e){
-		((JTextPane)MainRightClickMenu.e.getSource()).setEditable(true);
-		((JTextPane)MainRightClickMenu.e.getSource()).requestFocus();
+		((MainTextPane)MainRightClickMenu.eBox.getSource()).setContentType("text/plain");
+		((MainTextPane)MainRightClickMenu.eBox.getSource()).setEditable(true);
+		((MainTextPane)MainRightClickMenu.eBox.getSource()).requestFocus();
+	}
+}
+class FocusListener1 implements FocusListener{
+	private String string;
+	@Override
+	public void focusGained(FocusEvent e) {
+		if(((MainTextPane) e.getSource()).isEditable()){
+			string = ((MainTextPane) e.getSource()).getText();
+			
+		}
+	}
+	@Override
+	public void focusLost(FocusEvent e) {
+		string = ((MainTextPane) e.getSource()).getText();
+		((MainTextPane) e.getSource()).setEditable(false);
+		((MainTextPane) e.getSource()).setContentType("text/html");
+		((MainTextPane) e.getSource()).setText(unParse(string));
+	}
+	public String unParse(String in){
+		String[] BBCodeList  = {"\\[", "\\]", "color=", "/color", "size=", "/size", "\n"};
+		String[] HTMLCodeList = {"<", ">", "font color=", "/font", "font size=", "/font", "<br />"};
+		String B = in;
+		for(int y = 0; y < BBCodeList.length; y++){
+			String A[] = B.split(BBCodeList[y]);
+			B = A[0];
+			for(int x = 1; x < A.length; x++){
+				B += HTMLCodeList[y];
+				B += A[x];
+			}
+		}
+		return "<body style=\"font-family: " + Main.frame.getFont().getFamily() + "\">"+B+"</style>";
 	}
 }
