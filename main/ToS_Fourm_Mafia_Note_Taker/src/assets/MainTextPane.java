d907bce274e8ca655373e43e786382daeda11e30
Coolway99 <xxcoolwayxx@gmail.com>
2014-08-05 10:17:59 -0400

    Created a JTextPane that has a string attached, very useful

---
+++ main/ToS_Fourm_Mafia_Note_Taker/src/assets/MainTextPane.java
@@ -0,0 +1,25 @@
+package assets;
+
+import javax.swing.JTextPane;
+
+@SuppressWarnings("serial")
+public class MainTextPane extends JTextPane{
+	private String origString;
+	public MainTextPane(){
+		origString = null;
+	}
+	@Override
+	public void setEditable(boolean in){
+		super.setEditable(in);
+		if(in){
+			try{
+				this.setText(origString);
+			} catch(NullPointerException e){
+				
+			}
+		} else {
+			this.setContentType("text/plain");
+			origString = this.getText();
+		}
+	}
+}
