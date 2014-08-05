7c5849e264b7dfa2cea6af4ea9eedc20f261bb09
Coolway99 <xxcoolwayxx@gmail.com>
2014-08-05 12:29:11 -0400

    Put the title in titlebar

--- main/ToS_Fourm_Mafia_Note_Taker/src/main/Main.java
+++ main/ToS_Fourm_Mafia_Note_Taker/src/main/Main.java
@@ -67,7 +67,7 @@ public class Main {
 	public static int selectedDay = 1;
 	public static boolean isDay = true;
 	public static boolean fileSelected = false;
-	public static final String title = "Forum Mafia Note Taker Alpha V-In Dev-";
+	public static final String title = "Forum Mafia Note Taker V1.0";
 	
 	public static void main(String[] Args){
 		frame = new JFrame(title + " - new");
